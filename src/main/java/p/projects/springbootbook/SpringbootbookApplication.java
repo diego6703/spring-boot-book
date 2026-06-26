c7ee83f2c403bebb6cd2c9cc36e88701c63dbb88
Jakub Cybulski <jakub_cybulski@o2.pl>
2026-06-26 11:33:36 +0200

    chore(ci): fix trigger syntax in GitHub Actions workflow

---
+++ src/main/java/p/projects/springbootbook/SpringbootbookApplication.java
@@ -0,0 +1,13 @@
+package p.projects.springbootbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootbookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootbookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/p/projects/springbootbook/SpringbootbookApplicationTests.java
@@ -0,0 +1,13 @@
+package p.projects.springbootbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootbookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
